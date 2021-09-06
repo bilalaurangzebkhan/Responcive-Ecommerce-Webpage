39e9dfea8087cc33bc52d4cc3ac0cf67e69770ae
Bilal Autangzeb Khan <78090865+Thix-Bilz@users.noreply.github.com>
2021-09-06 11:50:09 +0500

    Add files via upload

---
+++ Mobera/js/app.js
@@ -0,0 +1,98 @@
+// Navbar
+window.addEventListener('resize', function(){
+  addRequiredClass();
+})
+
+function addRequiredClass() {
+  if (window.innerWidth < 860) {
+      document.body.classList.add('mobile')
+  } else {
+      document.body.classList.remove('mobile') 
+  }
+}
+
+window.onload = addRequiredClass
+
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger span')
+
+let isActive = false
+
+hamburger.addEventListener('click', function() {
+  mobileNav.classList.toggle('open')
+  if(!isActive) {
+      bars[0].style.transform = 'rotate(45deg)'
+      bars[1].style.opacity = '0'
+      bars[2].style.transform = 'rotate(-45deg)'
+      isActive = true
+  } else {
+      bars[0].style.transform = 'rotate(0deg)'
+      bars[1].style.opacity = '1'
+      bars[2].style.transform = 'rotate(0deg)'
+      isActive = false
+  }
+})
+
+//CARD
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
+
+// slider
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['a.png', 'b.png', 'c.png', 'd.png', 'e.png'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
